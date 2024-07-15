ffb32a8d18f78246f990b7449cd45ab4c242e1c8
d91 <dominik.tomcik23@gmail.com>
2024-07-15 13:53:51 +0200

    fix: update webscrape api

--- pages/api/get-instagram.ts
+++ pages/api/get-instagram.ts
@@ -1,11 +1,16 @@
 import puppeteer from "puppeteer";
 
+export const revalidate = 60 * 60 * 24;
+
 export default async function handler(req, res) {
+  const install = require(`puppeteer/internal/node/install.js`).downloadBrowser;
+  await install();
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto("https://c9d650605ea842e4b4bbde098da0b587.elf.site");
   await page.setViewport({ width: 1080, height: 1024 });
   await page.waitForSelector("a img");
+
   const imgs = await page.$$eval(
     ".eapps-instagram-feed-posts-item-link",
     (links) => {
