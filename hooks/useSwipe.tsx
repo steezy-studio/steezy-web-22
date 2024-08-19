3683e40473b7bf0947d8d61c1bbd152b3a03fecb
d91 <dominik.tomcik23@gmail.com>
2024-08-19 11:30:20 +0200

    fix: allow to scroll from slider

--- hooks/useSwipe.tsx
+++ hooks/useSwipe.tsx
@@ -15,28 +15,38 @@ export function useSwipe({ ref, cb, deps = [] }: UseSwipeProps) {
   useEffect(() => {
     if (!isTouchDevice()) return;
     let initialX: number | null = null;
-    const html = document.querySelector("html");
+    let initialY: number | null = null;
+
+    function handleTouchMove(e: TouchEvent) {
+      if (
+        Math.abs(e.changedTouches[0].clientY - initialY) <
+        Math.abs(e.changedTouches[0].clientX - initialX)
+      ) {
+        lenis?.stop();
+      }
+    }
 
     function handleTouchStart(e: TouchEvent) {
       initialX = e.changedTouches[0].clientX;
-      lenis?.stop();
+      initialY = e.changedTouches[0].clientY;
     }
 
     function handleTouchEnd(e: TouchEvent) {
+      lenis?.start();
       if (initialX === null) return;
       const swipeAmoumt = Math.abs(e.changedTouches[0].clientX - initialX);
-      html.style.overflow = "scroll";
       if (swipeAmoumt < 100) return;
       const direction = e.changedTouches[0].clientX - initialX > 0 ? -1 : 1;
       initialX === null;
-      lenis?.start();
       cb(direction);
     }
 
     ref.current?.addEventListener("touchstart", handleTouchStart);
+    ref.current?.addEventListener("touchmove", handleTouchMove);
     ref.current?.addEventListener("touchend", handleTouchEnd);
     return () => {
       ref.current?.removeEventListener("touchend", handleTouchStart);
+      ref.current?.removeEventListener("touchmove", handleTouchMove);
       ref.current?.removeEventListener("touchend", handleTouchEnd);
     };
   }, [isTouchDevice, lenis, ...deps]);
